0b275a0447881aebdee1b8a7be967df715d14f67
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-04-19 01:01:42 +0200

    Update gravityTask.php

--- src/gravity/refaltor/task/gravityTask.php
+++ src/gravity/refaltor/task/gravityTask.php
@@ -2,6 +2,7 @@
 
 namespace gravity\refaltor\task;
 
+use gravity\refaltor\events\playerListener;
 use gravity\refaltor\gravity;
 use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
@@ -26,33 +27,47 @@ class gravityTask extends Task
         $player = Server::getInstance()->getPlayerExact($this->_NAME);
         if (!empty($player))
         {
-            $path = gravity::getInstance()->getDataFolder();
-            $data = new Config($path . "players/" . $player->getName() . ".yml", 2);
-            $time = $data->get($player->getName());
+            $time = playerListener::$oxygene[$player->getName()];
             $world = gravity::getInstance()->getConfig()->get("world");
             if (in_array($player->getLevel()->getFolderName(), $world))
             {
                 $player->addEffect(new EffectInstance(Effect::getEffect(Effect::SPEED), 40, 1, false));
                 $player->addEffect(new EffectInstance(Effect::getEffect(Effect::LEVITATION), 40, -7, false));
                 $player->addEffect(new EffectInstance(Effect::getEffect(Effect::JUMP), 40, 3, false));
-                if ($time <= 5 && $time >= 1) {
-                    $player->sendPopup("§4[§c!§4]§e Oxygene §6§l»§r§c $time");
-                    $player->getLevel()->broadcastLevelEvent($player, LevelEventPacket::EVENT_SOUND_CLICK);
-                    $data->set($player->getName(), $data->get($player->getName()) - 1);
-                    $data->save();
-                }elseif ($time <= 0){
-                    if (!$player->isAlive()){} else {
-                    $player->sendPopup("§4[§c!§4]§e Oxygene §6§l»§r§4 alert");
-                    $player->setHealth($player->getHealth() - 1);
-                    $player->broadcastEntityEvent(ActorEventPacket::HURT_ANIMATION);
-                    $player->getLevel()->broadcastLevelSoundEvent($player, LevelSoundEventPacket::SOUND_HURT);
-                    $player->addEffect(new EffectInstance(Effect::getEffect(Effect::NAUSEA), 40, 3, false));
-                    }
-                }else{
-                    $data->set($player->getName(), $data->get($player->getName()) - 1);
-                    $data->save();
-                    $player->sendPopup("§eOxygene §6§l»§r§a $time");
-                }
+                $inv = $player->getArmorInventory();
+
+
+
+                if ($inv->getHelmet()->getId() !== gravity::getInstance()->getConfig()->get("astronaute_armor")["helmet"] ||
+					$inv->getChestplate()->getId() !== gravity::getInstance()->getConfig()->get("astronaute_armor")["chestplate"]||
+					$inv->getLeggings()->getId() !== gravity::getInstance()->getConfig()->get("astronaute_armor")["leggings"] ||
+					$inv->getBoots()->getId() !== gravity::getInstance()->getConfig()->get("astronaute_armor")["boots"])
+                {
+                	$player->addEffect(new EffectInstance(Effect::getEffect(Effect::BLINDNESS), 40, 1, false));
+                	$player->sendPopup("§4[§c!§4]§e Oxygene §6§l»§r§4 alert equipment not equipped ");
+					$player->setHealth($player->getHealth() - 1);
+					$player->broadcastEntityEvent(ActorEventPacket::HURT_ANIMATION);
+					$player->getLevel()->broadcastLevelSoundEvent($player, LevelSoundEventPacket::SOUND_HURT);
+				}else {
+                	
+					if ($time <= 5 && $time >= 1) {
+						$player->sendPopup("§4[§c!§4]§e Oxygene §6§l»§r§c $time");
+						$player->getLevel()->broadcastLevelEvent($player, LevelEventPacket::EVENT_SOUND_CLICK);
+						playerListener::$oxygene[$player->getName()] = playerListener::$oxygene[$player->getName()] - 1;
+					} elseif ($time <= 0) {
+						if (!$player->isAlive()) {
+						} else {
+							$player->sendPopup("§4[§c!§4]§e Oxygene §6§l»§r§4 alert");
+							$player->setHealth($player->getHealth() - 1);
+							$player->broadcastEntityEvent(ActorEventPacket::HURT_ANIMATION);
+							$player->getLevel()->broadcastLevelSoundEvent($player, LevelSoundEventPacket::SOUND_HURT);
+							$player->addEffect(new EffectInstance(Effect::getEffect(Effect::NAUSEA), 40, 3, false));
+						}
+					} else {
+						playerListener::$oxygene[$player->getName()] = playerListener::$oxygene[$player->getName()] - 1;
+						$player->sendPopup("§eOxygene §6§l»§r§a $time");
+					}
+				}
             }
         }else gravity::getInstance()->getScheduler()->cancelTask($this->getTaskId());
     }
