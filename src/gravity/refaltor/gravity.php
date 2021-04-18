bfa471f9005ecdbf24f76a885db19059ec17b4ed
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-04-19 01:24:57 +0200

    Update gravity.php

--- src/gravity/refaltor/gravity.php
+++ src/gravity/refaltor/gravity.php
@@ -9,6 +9,22 @@ use pocketmine\Server;
 class gravity extends PluginBase
 {
     private static $instance;
+    const CONFIG = [
+    	"version" => "2.0.0",
+		"O²_time_use" => 1,
+		"oxygene" => 40,
+		"oxygene_item" => "388:0",
+		"oxygene_add" => 30,
+		"astronaute_armor" => [
+			"helmet" => 314,
+			"chestplate" => 315,
+			"leggings" => 316,
+			"boots" => 317
+			],
+		"world" => [
+			"world"
+			]
+		];
 
     public static function getInstance(): gravity
     {
@@ -18,8 +34,21 @@ class gravity extends PluginBase
     public function onEnable()
     {
         self::$instance = $this;
-        @mkdir($this->getDataFolder() . "players/");
         $this->saveDefaultConfig();
         Server::getInstance()->getPluginManager()->registerEvents(new playerListener(), $this);
+        $this->fixOldConfig();
     }
+
+    private function fixOldConfig(): void
+	{
+		if ($this->getConfig()->exists("version")){
+			if ($this->getConfig()->get("version") !== "2.0.0")  {
+				$this->getConfig()->setAll(self::CONFIG);
+				$this->getConfig()->save();
+			}
+		}else {
+			$this->getConfig()->setAll(self::CONFIG);
+			$this->getConfig()->save();
+		}
+	}
 }
